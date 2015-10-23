b15aaefb286c57cbb382c2ebb57dceb4a4b5d2c8
Cristóbal Sánchez <cristobal.sanchez@coraltis.com>
2015-10-23 15:11:58 -0300

    Agrega boton que guarda los gastos en localstorage

--- www/js/app.js
+++ www/js/app.js
@@ -28,7 +28,13 @@ function runApp($ionicPlatform) {
 appCtrl.$inject = ['$scope', '$window', '$log', 'AppFactory', '$ionicLoading'];
 function appCtrl ( $scope, $window, $log, AppFactory, $ionicLoading) {
 
-    if ( !$window.localStorage.mxnInClp) {
+    if (!$window.localStorage.totalSpend) {
+        var spends = [];
+        spends.push("0");
+        $window.localStorage.totalSpend = JSON.stringify(spends);
+    }
+
+    if ( !$window.localStorage.mxnInClp ) {
         $window.localStorage.mxnInClp = 41;
     }
     $scope.mxnToCLP = $window.localStorage.mxnInClp;
@@ -71,6 +77,25 @@ function appCtrl ( $scope, $window, $log, AppFactory, $ionicLoading) {
         });
     };
 
+    $scope.saveSpend = function(){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        }
+        if(mm<10){
+            mm='0'+mm;
+        }
+        var todaySpend = "" + dd + mm + yyyy + "|" + $scope.outputCLP;
+        $log.debug(todaySpend);
+
+        var storedSpends = JSON.parse($window.localStorage.totalSpend);
+        storedSpends.push(todaySpend);
+        $window.localStorage.totalSpend = JSON.stringify(storedSpends);
+    };
 }
 
 factoryApp.$inject = ['$http'];
